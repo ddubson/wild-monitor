3f02599d9cae96dbc0245abf318f0d0eb4f84d87
Dmitriy Dubson <d.dubson@gmail.com>
2019-02-07 20:25:21 -0500

    Create Gradle project

---
+++ src/main/kotlin/wild/monitor/App.kt
@@ -0,0 +1,12 @@
+package wild.monitor
+
+class App {
+    val greeting: String
+        get() {
+            return "Hello world."
+        }
+}
+
+fun main(args: Array<String>) {
+    println(App().greeting)
+}
