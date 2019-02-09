4307fe49e25907c7bfdf10fe4dadb24d1b7fa4ac
Dmitriy Dubson <d.dubson@gmail.com>
2019-02-09 10:44:12 -0500

    Add web module boilerplate

---
+++ wild-monitor-web/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {PureComponent} from "react";
+
+class App extends PureComponent {
+    public render(): JSX.Element {
+        return (
+            <div>Hello World!</div>
+        );
+    }
+}
+
+export default App;
