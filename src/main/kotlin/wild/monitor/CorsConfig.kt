fb7ffee36fb0afe55f8152a5bedb1c691d6ad647
Dmitriy Dubson <d.dubson@gmail.com>
2019-02-11 21:39:27 -0500

    Add required manifest for pcf deploy

--- src/main/kotlin/wild/monitor/CorsConfig.kt
+++ src/main/kotlin/wild/monitor/CorsConfig.kt
@@ -1,5 +1,6 @@
 package wild.monitor
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -8,11 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 @EnableWebMvc
 class CorsConfig: WebMvcConfigurer {
+    @Value("\${cors.origins}")
+    lateinit var allowedOrigins: String
+
     override fun addCorsMappings(registry: CorsRegistry?) {
         registry!!
                 .addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedOrigins("http://localhost:1234")
+                .allowedOrigins(allowedOrigins)
                 .allowedHeaders("*")
     }
 }
--- wild-monitor-web/src/App.config.ts
+++ wild-monitor-web/src/App.config.ts
@@ -1,6 +1,7 @@
 import axios, {AxiosInstance} from "axios";
+require('dotenv').config();
 
-const wildMonitorServiceUrl = process.env.WILD_MONITOR_SVC_URL || "http://localhost:8080";
+const wildMonitorServiceUrl = process.env.WILDMONITOR_API_URI || "http://localhost:8080";
 
 const wildMonitorService: AxiosInstance = axios.create({
     baseURL: wildMonitorServiceUrl,
